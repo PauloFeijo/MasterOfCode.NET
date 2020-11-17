404cd268ff0c1d432afc250c4faa2e12a244eacd
PauloFeijo <paulorikrdo.89@gmail.com>
2020-11-16 23:13:35 -0300

    Exercicio 3 - Trabalhando no console

---
+++ Trabalhando no console/Exercicio 3/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Faça uma aplicação que imprima todos os múltiplos de 3, entre 1 e 100. Utilize a repetição while. */
+
+using System;
+
+namespace Exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+            while (numero <= 100)
+            {
+                if (numero % 3 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+                numero++;
+            }
+        }
+    }
+}
